2e05467e14a73d5b41d5080ca8df3cfa64d04b45
Monky518 <zoeyrizor@gmail.com>
2023-09-17 17:42:58 -0400

    Final commit and push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //A program that starts with a credit card balance of $5000
+        //Then calculates the interest at a rate of 17%
+        //Assume the user does not make any payments
+        //Display the interest due after one month and again after two months
+
+        double cardBalance = 5000;
+        double interestRate = 1.17;
+
+        cardBalance = cardBalance * interestRate;
+        System.out.println("Your balance due after one month is " + cardBalance);
+
+        cardBalance = cardBalance * interestRate;
+        System.out.println("Your balance due after two months is " + cardBalance);
     }
 }
